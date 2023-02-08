c180e8dcff3221ab6baaf2e9c1a130698c3a0269
dmenjim072 <dmenjim072@g.educaand.es>
2023-02-08 14:43:17 +0100

    asd

---
+++ src/main/java/Recursividad/Fibonaci.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Recursividad;
+
+/**
+ *
+ * @author david
+ */
+public class Fibonaci {
+
+    public static int factorial(int num){
+        
+        if(num == 0 || num == 1){
+            
+            return 1;
+            
+        }else{
+            
+            return num*factorial(num-1);
+            
+        }
+        
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        
+        System.out.println(factorial(3));
+        
+    }
+}
